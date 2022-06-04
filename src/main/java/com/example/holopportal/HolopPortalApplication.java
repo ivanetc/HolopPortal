fe5640ec31f89daf591f1c68ba9d112d78c93728
ivanetc <ivanetsas@yandex.ru>
2022-06-04 19:46:19 +0300

    Initial commit

---
+++ src/main/java/com/example/holopportal/HolopPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.holopportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolopPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolopPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/holopportal/HolopPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holopportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolopPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
