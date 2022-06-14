7e8fe7af71057750e777e421efbb1f1860706e23
Егор Дмитриевич Хилик <khilik.egor@niuitmo.ru>
2022-06-14 16:13:36 +0300

    add PredictionController.java and prediction.html page

---
+++ src/main/java/com/example/holopportal/controllers/PredictionController.java
@@ -0,0 +1,25 @@
+package com.example.holopportal.controllers;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import com.example.holopportal.user.entities.User;
+import com.example.holopportal.user.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PredictionController {
+
+    @Inject
+    UserService userService;
+
+    @GetMapping("/prediction")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("currentUser", userService.getCurrentUser().get());
+        return "prediction";
+    }
+}
