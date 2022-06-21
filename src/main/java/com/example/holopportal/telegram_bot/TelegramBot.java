827a336b3d1662c94900ae583b3ecffb1dcebc22
Егор Дмитриевич Хилик <khilik.egor@niuitmo.ru>
2022-06-21 12:59:22 +0300

    add TelegramBot

--- src/main/java/com/example/holopportal/telegram_bot/TelegramBot.java
+++ src/main/java/com/example/holopportal/telegram_bot/TelegramBot.java
@@ -1,32 +1,46 @@
 package com.example.holopportal.telegram_bot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
+import java.util.List;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
-    @Value("${bot.username}")
-    private String username;
+    @Override
+    public String getBotUsername() {return "holop_itmo_bot";}
 
-    @Value("${bot.token}")
-    private String token;
+    @Override
+    public String getBotToken() {return "5451762902:AAHYO-oLRHuPSNTHtcBLNYAG_PWYR_nnygE";}
+
+    @Override
+    public void onRegister() {
+
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
 
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
-    public String getBotUsername() {
-        return username;
-    }
+    public void onUpdatesReceived(List<Update> updates) {
 
-    @Override
-    public String getBotToken() {
-        return token;
     }
 }
