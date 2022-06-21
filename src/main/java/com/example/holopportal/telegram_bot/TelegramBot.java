66eb3efbffa07ac23fe51f5f1ca1f2fd1808c79a
Егор Дмитриевич Хилик <khilik.egor@niuitmo.ru>
2022-06-21 09:47:02 +0300

    add TelegramBot

---
+++ src/main/java/com/example/holopportal/telegram_bot/TelegramBot.java
@@ -0,0 +1,32 @@
+package com.example.holopportal.telegram_bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
