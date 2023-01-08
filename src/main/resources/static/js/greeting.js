3767b47d1007732cb5b7c9907404ad6ef2828167
ivanetsas <ivanetsas@yandex.ru>
2023-01-08 22:07:26 +0200

    add styles

--- src/main/resources/static/js/greeting.js
+++ src/main/resources/static/js/greeting.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+    $('select').formSelect();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge: "right"});
+});
+
 console.log("greeting.js loaded")
