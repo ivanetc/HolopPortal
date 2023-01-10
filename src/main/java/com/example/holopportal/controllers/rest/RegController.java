dc8678cc045cb9ed54b563d44f8b1f2d59b819be
egor <khilik.egor@gmail.com>
2023-01-10 19:27:34 +0300

    Merge remote-tracking branch 'origin/refactoring_front' into refactoring_front
    # Conflicts:
    #	src/main/resources/templates/fragments/singleTask.html
    #	src/main/resources/templates/fragments/textEditor.html
    #	src/main/resources/templates/newscreenplay.html
    #	src/main/resources/templates/newtask.html

--- src/main/java/com/example/holopportal/controllers/rest/RegController.java
+++ src/main/java/com/example/holopportal/controllers/rest/RegController.java
@@ -1,8 +1,9 @@
-package com.example.holopportal.controllers.routing;
+package com.example.holopportal.controllers.rest;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegController {
